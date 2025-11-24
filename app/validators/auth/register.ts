625ddb4fd06944f92a584b1f227292933499c39d
vergnesl <lorick.vergnes@etu.umontpellier.fr>
2025-11-24 12:16:26 +0100

    feat : mise en place de l'email unique par utilisateur

--- app/validators/auth/register.ts
+++ app/validators/auth/register.ts
@@ -1,9 +1,17 @@
 import vine from '@vinejs/vine'
+import User from '#models/user'
 
 export const registerValidator = vine.compile(
   vine.object({
     fullName: vine.string().minLength(3).maxLength(255),
-    email: vine.string().email().normalizeEmail(),
+    email: vine
+      .string()
+      .email()
+      .normalizeEmail()
+      .unique(async (db, value) => {
+        const user = await User.findBy('email', value)
+        return !user
+      }),
     password: vine.string().minLength(8).maxLength(255),
   })
 )
